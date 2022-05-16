7f835ac3a2fbebe2a3202c7df353dfb7adbbb310
Michael Ding <michaelding@michaels-mbp.lan>
2022-05-16 13:37:10 -0400

    0.0.0.0

--- main/settings.py
+++ main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_i9i8^h((g!*&bbf_o63g=$xcue7avpdgad4ax%pqhlbkbmhp+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
