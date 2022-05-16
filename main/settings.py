25a09258891a0a21cb19449368945b8e2eef1a12
Michael Ding <michaelding@michaels-mbp.lan>
2022-05-16 15:29:24 -0400

    set debug to true

--- main/settings.py
+++ main/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_i9i8^h((g!*&bbf_o63g=$xcue7avpdgad4ax%pqhlbkbmhp+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
