c88ebbaacff049819861dbf395c4f9d5f2101385
Michael Ding <michaelding@Michaels-MacBook-Pro.local>
2022-05-15 22:18:17 -0400

    complete migration and add crud structure

--- main/settings.py
+++ main/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # class name of items
+    'items.apps.ItemsConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'], # add shared html
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
